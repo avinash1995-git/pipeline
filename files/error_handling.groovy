587a7dbb669911054a0e31b0d33edea979d73853
Avinash <avinashv255@gmail.com>
2022-08-15 16:37:16 +0530

    error handler added

--- files/error_handling.groovy
+++ files/error_handling.groovy
@@ -7,11 +7,21 @@ pipeline {
         stage ('Build') {
 
             steps {
+
+                catchError(buildResult: 'SUCCESS', message: 'This is known error', stageResult: 'FAILURE') {
                 sh '''
                 echo "this is my build stage"
 
                 exit 1
                 '''
+                }
+            }
+        }
+
+        stage('Commit') {
+
+            steps {
+                echo "this is mt commit stage"
             }
         }
         
